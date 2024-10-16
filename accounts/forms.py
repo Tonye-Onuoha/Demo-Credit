383a8754ab1fdc01ad051382bfcbc56039c0bc97
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2024-10-16 12:46:39 -0700

    Created registration form in accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegisterForm(UserCreationForm):
+    usable_password = None
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+        
