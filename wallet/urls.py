6435098a63156231a8438632d1d898348d267c18
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2024-10-17 02:39:23 -0700

    Updated url patterns for wallet app

---
+++ wallet/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home'),
+    path('create-default/', views.default_wallet, name='create-default-wallet'),
+    path('create-custom/', views.custom_wallet, name ='create-custom_wallet'),
+    path('fund-new/', views.new_funds, name='fund-new'),
+    path('withdraw-funds/', views.withdraw_funds, name='withdraw-funds'),
+    path('transfer-funds/', views.transfer_funds, name='transfer-funds'),
+    path('user-transactions/', views.UserTransactionsView.as_view(), name='user-transactions'),
+]
